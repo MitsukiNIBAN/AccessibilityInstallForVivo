de7919a01c3bec1fcca4b2b5f5e420f2ce6b4c1d
wangzc <wangzc@zcitc.com>
2019-06-18 10:31:48 +0800

    修改MainActivity描述

--- app/src/main/java/com/mitsuki/vivo/autoinstall/MainActivity.kt
+++ app/src/main/java/com/mitsuki/vivo/autoinstall/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.mitsuki.vivo.autoinstall
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn.setOnClickListener {
+            startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
+        }
     }
 }
